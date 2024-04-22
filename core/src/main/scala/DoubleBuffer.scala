2c445ec2785ab2cf47b56368c5eecc8fa3635abc
ZiluTian <zilu.tian@uzh.ch>
2024-04-22 11:45:17 +0200

    Update (correct comment)

--- core/src/main/scala/DoubleBuffer.scala
+++ core/src/main/scala/DoubleBuffer.scala
@@ -1,25 +1,26 @@
 package BSPModel
 
+import scala.quoted.* 
+
 // DoubleBuffer is an optimization applied to a BSP
 // todo: restrict publicState to have read-only access to other BSPs but read-write only by the owner BSP
 trait DoubleBuffer {
-    this: BSP with ComputeMethod =>
+    this: BSP & ComputeMethod =>
     var publicState: Message = stateToMessage(state)
     
-    // compile-time staging
+    // runtime staging (JIT compiled per run)
     val stagedComputation: Option[StagedExpr]
 
     def updatePublicState(): Unit = {
         publicState = stateToMessage(state)
     }
     
-    // todo: runtime staging to evaluate stagedComputation only once
     override def run(ms: List[Message]): Unit = {
         stagedComputation match {
             case None => state = run(state,  ms)
             case Some(x) => {
                 val stagedRes = x.compile()
-                println("staged expression evaluates to " + stagedRes)
+                // println("staged expression evaluates to " + stagedRes)
                 state = run(state, stagedRes.asInstanceOf[Message] :: ms)
             }
         }
@@ -28,7 +29,7 @@ trait DoubleBuffer {
 
 // todo, achieve so with reflection, to avoid creating a new object
 object DoubleBuffer {
-    def fromBSP(b: BSP with ComputeMethod): BSP with ComputeMethod with DoubleBuffer = {
+    def fromBSP(b: BSP & ComputeMethod): BSP & ComputeMethod & DoubleBuffer = {
         new BSP with ComputeMethod with DoubleBuffer {
 
             type State = b.State
